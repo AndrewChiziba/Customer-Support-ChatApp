0bb370dea5a3e3a5d243b904e43a2572a47aee95
Andrew Chiziba <chizibaandrew@gmail.com>
2022-01-09 03:04:48 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloFuture.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
